cb1f8b782ec1d92c8a475b03d39a513c23144992
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-09-20 19:50:40 -0700

    Add Elderberries

--- simple.js
+++ simple.js
@@ -1,12 +1,14 @@
 import { AppleModule } from "apple-module";
 import { BananaModule } from "banana-module";
 import { CherryModule } from "cherry-module";
+import { ElderberryModule } from "elderberry-module";
 
 @NgModule({
   imports: [
     AppleModule,
     BananaModule,
     CherryModule,
+    ElderberryModule,
   ],
 })
 export class MyModule {}
