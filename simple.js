7bf9dd0d1fdb7dca279be01c6eb665d3e00961ba
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2021-09-20 19:43:12 -0700

    My feature: Import DurianModule

--- simple.js
+++ simple.js
@@ -1,11 +1,13 @@
 import { AppleModule } from "apple-module";
 import { BananaModule } from "banana-module";
+import { DurianModule } from "durian-module";
 
 @NgModule({
   imports: [
     AppleModule,
     BananaModule,
     CherryModule,
+    DurianModule,
   ],
 })
 export class MyModule {}
